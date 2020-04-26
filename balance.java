a8dc81403666b28d6871ccd5f4ecdb2c19082e03
tatanalosinina <tatana@localhost>
2020-04-26 19:13:01 +0300

    first commit

---
+++ balance.java
@@ -0,0 +1,10 @@
+public class home {
+    public static void main(String[] args) {
+        int balanc = 2000000000;
+        System.out.println("balanc" + balanc);
+        int credit = 500000000;
+        System.out.println("credit" + credit);
+        int total = balanc * credit;
+        System.out.println("total" + total);
+    }
+}
